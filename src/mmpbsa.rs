30414af6a002c256c9e23b96f7fd79ce343bea6f
Supernova <Jiaxing_Zhang@outlook.com>
2022-10-20 19:37:15 +0800

    Added time period of trajectory extract, added dt to skip.

--- src/mmpbsa.rs
+++ src/mmpbsa.rs
@@ -7,22 +7,28 @@ pub fn do_mmpbsa_calculations(trj: &String, tpr: &String, ndx: &Index, use_dh: b
     let receptor = &ndx.groups[receptor_grp].indexes;
     let ligand = &ndx.groups[ligand_grp].indexes;
     let trj = XTCTrajectory::open_read(trj).expect("Error reading trajectory");
-    let atoms = get_atoms_trj(trj, ligand, 0.0, 0.0);
+    let atoms = get_atoms_trj(trj, ligand, 9501.0, 10000.0, 500.0);
     for i in 0..atoms.len() {
-        println!("frame {}: {:?}", i, atoms[i]);
+        println!("frame {}: {:?}\n", i, atoms[i]);
     }
 }
 
-fn get_atoms_trj(trj: XTCTrajectory, at_ids: &Vec<i32>, begin: f64, end: f64) -> Vec<Vec<[f32; 3]>> {
-    let mut coords:Vec<Vec<[f32; 3]>> = vec![];
-    for (idx, result) in trj.into_iter().enumerate() {
-        // 增加时间限制
-        let mut c: Vec<[f32; 3]> = vec![[0.0; 3]; at_ids.len()];
+fn get_atoms_trj(trj: XTCTrajectory, at_ids: &Vec<i32>, begin: f64, end: f64, dt: f64) -> Vec<Vec<[f32; 3]>> {
+    let mut coord_matrix:Vec<Vec<[f32; 3]>> = vec![];
+    let mut start_t = 0.0;
+    for result in trj.into_iter() {
         let frame = result.unwrap();
-        for i in 0..at_ids.len() {
-            c[i] = frame.coords[at_ids[i] as usize];
+        let ts: f64 = frame.time as f64;
+        if ts > begin && start_t == 0.0 {
+            start_t = ts;
         }
-        coords.push(c);
+        let mut c: Vec<[f32; 3]> = vec![[0.0; 3]; at_ids.len()];
+        if ts >= begin && ts <= end && (ts - start_t) % dt == 0.0 {
+            for i in 0..at_ids.len() {
+                c[i] = frame.coords[at_ids[i] as usize];
+            }
+            coord_matrix.push(c);
+        }
     }
-    return coords;
+    return coord_matrix;
 }
