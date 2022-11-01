9332a6479ea0d645ee2da831e0520a39560260ef
Supernova <Jiaxing_Zhang@outlook.com>
2022-11-01 22:08:50 +0800

    Fixed search parameters.

--- src/mmpbsa.rs
+++ src/mmpbsa.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 use crate::index_parser::Index;
 use xdrfile::*;
 use crate::Parameters;
-use ndarray::{array, Array, ArrayBase, Dim, Ix, NdIndex, OwnedRepr};
+use ndarray::{array, Array, ArrayBase, Dim, Ix, NdIndex, OwnedRepr, Shape};
 use fs::File;
 use std::cmp::max;
 use std::io::Write;
@@ -61,9 +61,9 @@ fn get_atoms_trj(trj: XTCTrajectory) -> (ArrayBase<OwnedRepr<[f32; 3]>, Dim<[Ix;
         num_frames += 1;
         num_atoms = frame.num_atoms();
     }
-    let coord_matrix = ndarray::Array::from_shape_vec(
+    let coord_matrix = Array::from_shape_vec(
         (num_frames, num_atoms), coord_matrix).unwrap();
-    let box_size = ndarray::Array::from_shape_vec(
+    let box_size = Array::from_shape_vec(
         (num_frames, 3), box_size).unwrap();
     return (coord_matrix, box_size);
 }
@@ -79,7 +79,7 @@ fn gen_qrv(mdp: &String, ndx: &Index, wd: &Path, receptor_grp: usize, ligand_grp
 
     // get MD parameters
     let re = Regex::new(r"\s*ffparams:").unwrap();
-    let locator = get_md_locators(&mdp, &re)[0];
+    let locator = get_md_locators_all(&mdp, &re)[0];
     // number of atom types
     let re = Regex::new(r"\s*atnr=(\d+)").unwrap();
     let atnr = re.captures(mdp[locator + 1]).unwrap();
@@ -92,51 +92,81 @@ fn gen_qrv(mdp: &String, ndx: &Index, wd: &Path, receptor_grp: usize, ligand_grp
     let mut epsilon: Vec<f64> = vec![0.0; atnr];
     let mut rad: Vec<f64> = vec![rad_lj0; atnr];
 
-    println!("Generating qrv file...");
-    for i in 0..atnr {
-        qrv.write_all(format!("{:6}", i).as_bytes()).expect("Writing qrv file failed");
-        // 获取每个原子的C6和C12
-        for j in 0..atnr {
-            let re = Regex::new(r".*c6\s*=\s*(.*),.*").unwrap();
-            let re = Regex::new(r".*c12\s*=\s*(.*?)\s*$").unwrap();
-            let c6 = re.captures(mdp[locator]).unwrap().get(1).unwrap().as_str();
-            let c12 = re.captures(mdp[locator]).unwrap().get(1).unwrap().as_str();
-            qrv.write_all(format!(" {} {}", c6, c12).as_bytes()).expect("Writing qrv file failed");
-            let c6: f64 = c6.parse().unwrap();
-            let c12: f64 = c12.parse().unwrap();
-            // 计算每种原子类型的σ, ε, 半径
-            if j == i {
-                sigma[i] = 0.0;
-                epsilon[i] = 0.0;
-                rad[i] = rad_lj0;
-                if c6 * c12 != 0.0 {
-                    sigma[i] = 10.0 * (c12 / c6).powf(1.0 / 6.0); // 转换单位为A
-                    epsilon[i] = c6.powi(2) / (4.0 * c12);
-                    rad[i] = 0.5 * sigma[i]; // sigma为直径
-                }
-            }
-        }
-        locator += 1;
-        qrv.write_all("\n".as_bytes()).expect("Writing qrv file failed");
-    }
-    println!("Finished generating qrv file.");
+    // println!("Generating qrv file...");
+    // for i in 0..atnr {
+    //     qrv.write_all(format!("{:6}", i).as_bytes()).expect("Writing qrv file failed");
+    //     // 获取每个原子的C6和C12
+    //     for j in 0..atnr {
+    //         let re = Regex::new(r".*c6\s*=\s*(.*),.*").unwrap();
+    //         let re = Regex::new(r".*c12\s*=\s*(.*?)\s*$").unwrap();
+    //         let c6 = re.captures(mdp[locator]).unwrap().get(1).unwrap().as_str();
+    //         let c12 = re.captures(mdp[locator]).unwrap().get(1).unwrap().as_str();
+    //         qrv.write_all(format!(" {} {}", c6, c12).as_bytes()).expect("Writing qrv file failed");
+    //         let c6: f64 = c6.parse().unwrap();
+    //         let c12: f64 = c12.parse().unwrap();
+    //         // 计算每种原子类型的σ, ε, 半径
+    //         if j == i {
+    //             sigma[i] = 0.0;
+    //             epsilon[i] = 0.0;
+    //             rad[i] = rad_lj0;
+    //             if c6 * c12 != 0.0 {
+    //                 sigma[i] = 10.0 * (c12 / c6).powf(1.0 / 6.0); // 转换单位为A
+    //                 epsilon[i] = c6.powi(2) / (4.0 * c12);
+    //                 rad[i] = 0.5 * sigma[i]; // sigma为直径
+    //             }
+    //         }
+    //     }
+    //     locator += 1;
+    //     qrv.write_all("\n".as_bytes()).expect("Writing qrv file failed");
+    // }
+    // println!("Finished generating qrv file.");
+
+    // number of systems(?)
     let re = Regex::new(r"\s*#molblock\s*=\s*(.+?)\s*").unwrap();
-    let mol_num: Vec<i32> = get_md_params(&mdp, &re);
-    let mol_num = mol_num[0];
+    let mol_num: i32 = get_md_params_first(&mdp, &re).parse().unwrap();
+    // number of molecule types
     let re = Regex::new(r"\s*moltype\s*=\s*(.+?)\s*").unwrap();
-    let Imol: Vec<i32> = get_md_params(&mdp, &re);
+    let Imol: Vec<i32> = get_md_params_all(&mdp, &re);
+    // number of molecules
     let re = Regex::new(r"\s*#molecules\s*=\s*(.+?)\s*").unwrap();
-    let Nmol: Vec<i32> = get_md_params(&mdp, &re);
+    let Nmol: Vec<i32> = get_md_params_all(&mdp, &re);
+    // number of atoms
     let re = Regex::new(r"atom \((.+)\):").unwrap();
-    let Natm: Vec<i32> = get_md_params(&mdp, &re);
-    println!("{}", Natm.iter().max().unwrap());
+    let Natm: Vec<i32> = get_md_params_all(&mdp, &re);
+    let maxNatm = Natm.iter().max().unwrap();
     // Name = Vector{String}()
     // Natm = Vector{Int32}()
+
+    // locator of molecule types
+    let re = Regex::new(r"\s*moltype.+\(").unwrap();
+    let locators = get_md_locators_all(&mdp, &re);
+
+
+    // 先看看ndarray再决定下面怎么写
+    let arr: ArrayBase<OwnedRepr<[f32; 3]>, Dim<[Ix; 2]>> = Array::zeros((3, 100));
+    println!("{:?}", arr);
+    // let resID= Array::zeros();
+
+    // Catm = zeros(Int32, length(Nmol), maxNatm)
+    // Ratm = zeros(Float64, length(Nmol), maxNatm)
+    // Satm = zeros(Float64, length(Nmol), maxNatm)
+    // Eatm = zeros(Float64, length(Nmol), maxNatm)
+    // Qatm = zeros(Float64, length(Nmol), maxNatm)
+    // Tatm = Array{String}(undef, length(Nmol), maxNatm)
 
     // get atom parameters
 }
 
-fn get_md_locators(strings: &Vec<&str>, re: &Regex) -> Vec<usize> {
+fn get_md_locators_first(strings: &Vec<&str>, re: &Regex) -> usize {
+    for (idx, l) in strings.into_iter().enumerate() {
+        if re.is_match(l) {
+            return idx;
+        }
+    }
+    return 0;
+}
+
+fn get_md_locators_all(strings: &Vec<&str>, re: &Regex) -> Vec<usize> {
     let mut locators: Vec<usize> = vec![];
     for (idx, l) in strings.into_iter().enumerate() {
         if re.is_match(l) {
@@ -146,7 +176,18 @@ fn get_md_locators(strings: &Vec<&str>, re: &Regex) -> Vec<usize> {
     return locators;
 }
 
-fn get_md_params<T: Debug + FromStr>(strings: &Vec<&str>, re: &Regex) -> Vec<T> where <T as FromStr>::Err: Debug {
+fn get_md_params_first(strings: &Vec<&str>, re: &Regex) -> String {
+    for line in strings {
+        if re.is_match(line) {
+            let value = re.captures(line).unwrap();
+            let value = value.get(1).unwrap().as_str();
+            return value.to_string();
+        }
+    }
+    return String::new();
+}
+
+fn get_md_params_all<T: Debug + FromStr>(strings: &Vec<&str>, re: &Regex) -> Vec<T> where <T as FromStr>::Err: Debug {
     let mut values: Vec<T> = vec![];
     for line in strings {
         if re.is_match(line) {
