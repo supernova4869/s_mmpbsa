7870bd06036f438f34f0c1cdba7a22457c9fa372
Supernova <Jiaxing_Zhang@outlook.com>
2022-11-05 20:05:51 +0800

    Read parameters from qrv.

--- src/mmpbsa.rs
+++ src/mmpbsa.rs
@@ -5,7 +5,10 @@ use xdrfile::*;
 use crate::Parameters;
 use ndarray::{Array, Array1, Array2, ArrayBase, Dim, Ix, NdIndex, OwnedRepr, Shape};
 use std::cmp::max;
+use std::fs::File;
 use std::io::stdin;
+use std::ops::Add;
+use regex::Regex;
 use crate::gen_qrv::gen_qrv;
 
 pub fn do_mmpbsa_calculations(trj: &String, mdp: &String, ndx: &Index, wd: &Path, use_dh: bool, use_ts: bool,
@@ -17,12 +20,19 @@ pub fn do_mmpbsa_calculations(trj: &String, mdp: &String, ndx: &Index, wd: &Path
     //     get_atoms_trj(trj);   // frames x atoms(3x1)
     // pbc whole 先不写, 先默认按照已经消除了周期性来做后续处理, 之后再看周期性的事
     // 2. get charge, radius, LJ parameters of each atoms and generate qrv files
-    // let qrv_path = "_Protein_in_water.qrv";
-    // let qrv_path = wd.join(qrv_path);
-    // let qrv_path = qrv_path.to_str().unwrap();
+    let mut pid = String::from("_Protein_in_water");
+    println!("Input system name (default: {}):", pid);
+    let mut input = String::new();
+    stdin().read_line(&mut input).expect("Error input");
+    if input.trim().len() != 0 {
+        pid = input.trim().to_string();
+    }
+    let qrv_path = String::from(pid.as_str()) + ".qrv";
+    let qrv_path = wd.join(qrv_path);
+    let qrv_path = qrv_path.to_str().unwrap();
     // gen_qrv(mdp, ndx, receptor_grp, ligand_grp, qrv_path, rad_type, rad_lj0);
     // 3. Mpdb>pqr, 输出apbs, 计算MM, APBS
-    do_mmpbsa(trj, mdp, ndx, wd, complex_grp, receptor_grp, ligand_grp, settings);
+    do_mmpbsa(trj, mdp, ndx, qrv_path, wd, pid, complex_grp, receptor_grp, ligand_grp, settings);
 }
 
 fn get_atoms_trj(trj: XTCTrajectory) -> (ArrayBase<OwnedRepr<[f32; 3]>, Dim<[Ix; 2]>>,
@@ -50,7 +60,7 @@ fn get_atoms_trj(trj: XTCTrajectory) -> (ArrayBase<OwnedRepr<[f32; 3]>, Dim<[Ix;
     return (coord_matrix, box_size);
 }
 
-fn do_mmpbsa(trj: &String, mdp: &String, ndx: &Index, wd: &Path, complex_grp: usize, receptor_grp: usize, ligand_grp: usize, settings: &Parameters) {
+fn do_mmpbsa(trj: &String, mdp: &String, ndx: &Index, qrv: &str, wd: &Path, pid: String, complex_grp: usize, receptor_grp: usize, ligand_grp: usize, settings: &Parameters) {
     // Running settings
     let gmx = &settings.gmx;
     let apbs = &settings.apbs;
@@ -62,9 +72,6 @@ fn do_mmpbsa(trj: &String, mdp: &String, ndx: &Index, wd: &Path, complex_grp: us
     let fadd = &settings.fadd;
     let df = &settings.df;
 
-    let complex = &ndx.groups[complex_grp].indexes;
-    let receptor = &ndx.groups[receptor_grp].indexes;
-    let ligand = &ndx.groups[ligand_grp].indexes;
     // PB部分的参数从哪来?是否从前一菜单转移?
     let PBEset = "\
 temp  298.15      # 温度\
@@ -117,17 +124,72 @@ grid  0.1 0.1 0.1\
 \
 calcforce no\
 calcenergy total";
-    let mut pid = "_system";
-    println!("Input system name (default: {}):", pid);
-    let mut input = String::new();
-    stdin().read_line(&mut input).expect("Error input");
-    if input.len() != 0 {
-        pid = input.trim();
-    }
+    let qrv = wd.join(pid.to_string() + ".qrv");
+    println!("{}", qrv.display());
     let wd = wd.join("temp");
     println!("Temporary files will be placed at {}", wd.display());
-    let err = wd.join("$pid.err");
-    let qrv = wd.join("$pid.qrv");
-    let pdb = wd.join("$pid.pdb");
+    let err = wd.join(pid.to_string() + ".err");
+    let pdb = wd.join(pid.to_string() + ".pdb");
     println!("0. Finished setting up environments and parameters.");
+
+    // run MM-PBSA calculatons
+    println!("Running MM-PBSA calculatons...");
+    let qrv = fs::read_to_string(qrv).unwrap();
+    let qrv: Vec<&str> = qrv.split("\n").collect();
+    // c6 and c12
+    let Atyp = qrv[1];
+    let Atyp: usize = Atyp.trim().parse().unwrap();
+    let mut C6 = Array2::<f64>::zeros((Atyp, Atyp));
+    let mut C12 = Array2::<f64>::zeros((Atyp, Atyp));
+    for i in 0..Atyp {
+        for j in 0..Atyp {
+            let paralj: Vec<&str> = qrv[i + 2].trim().split(" ").collect();
+            let c6: f64 = paralj[2 * j].parse().unwrap();
+            C6[[i, j]] = c6;
+            let c12: f64 = paralj[2 * j + 1].parse().unwrap();
+            C12[[i, j]] = c12;
+        }
+    }
+
+    let ndxCom = &ndx.groups[complex_grp].indexes;
+    let ndxPro = &ndx.groups[receptor_grp].indexes;
+    let ndxLig = &ndx.groups[ligand_grp].indexes;
+    let mut Qatm = Array1::<f64>::zeros(qrv.len() - 3 - Atyp);
+    let mut Ratm = Array1::<f64>::zeros(qrv.len() - 3 - Atyp);
+    let mut Catm = Array1::<i32>::zeros(qrv.len() - 3 - Atyp);
+    let mut Satm = Array1::<f64>::zeros(qrv.len() - 3 - Atyp);
+    let mut Eatm = Array1::<f64>::zeros(qrv.len() - 3 - Atyp);
+    // 考虑将这两个合并成一个
+    let mut resPro = Array1::<String>::default(qrv.len() - 3 - Atyp);
+    let mut resLig = Array1::<String>::default(qrv.len() - 3 - Atyp);
+    let mut idx = 0;
+    for line in &qrv[Atyp + 2..qrv.len() - 1] { // there's a blank line at the end
+        let line: Vec<&str> = line
+            .trim()
+            .split(" ")
+            .filter_map(|p| match p.len() {
+                0 => None,
+                _ => Some(p)
+            })
+            .collect();
+        Qatm[idx] = line[1].parse().unwrap();
+        Ratm[idx] = line[2].parse().unwrap();
+        Catm[idx] = line[3].parse().unwrap();
+        Satm[idx] = line[4].parse().unwrap();
+        Eatm[idx] = line[5].parse().unwrap();
+
+        if line[line.len() - 1] == "Pro" {
+            resPro[idx] = "P~".to_string() + line[line.len() - 3];
+        } else {
+            resLig[idx] = "L~".to_string() + line[line.len() - 3];
+        }
+        idx += 1;
+        println!("{:?}", Qatm);
+        println!("{:?}", Ratm);
+        println!("{:?}", Catm);
+        println!("{:?}", Satm);
+        println!("{:?}", Eatm);
+        println!("{:?}", resPro);
+        println!("{:?}", resLig);
+    }
 }
