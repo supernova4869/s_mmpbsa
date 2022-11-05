8d8cbf016b07f4de65094e81f76e61d534a144c3
Supernova <Jiaxing_Zhang@outlook.com>
2022-11-06 00:48:01 +0800

    Optimized reading frames logic.

--- src/mmpbsa.rs
+++ src/mmpbsa.rs
@@ -3,11 +3,12 @@ use std::path::Path;
 use crate::index_parser::Index;
 use xdrfile::*;
 use crate::Parameters;
-use ndarray::{Array, Array1, Array2, ArrayBase, Dim, Ix, NdIndex, OwnedRepr, Shape};
+use ndarray::{Array, Array1, Array2, Array3, ArrayBase, Dim, Ix, NdIndex, OwnedRepr, Shape};
 use std::cmp::max;
 use std::fs::File;
 use std::io::stdin;
 use std::ops::Add;
+use std::rc::Rc;
 use regex::Regex;
 use crate::gen_qrv::gen_qrv;
 
@@ -15,10 +16,12 @@ pub fn do_mmpbsa_calculations(trj: &String, mdp: &String, ndx: &Index, wd: &Path
                               complex_grp: usize, receptor_grp: usize, ligand_grp: usize,
                               bt: f64, et: f64, dt: f64, settings: &Parameters) {
     // 1. 预处理轨迹: 复合物完整化, 团簇化, 居中叠合, 然后生成pdb文件
-    // let trj = XTCTrajectory::open_read(trj).expect("Error reading trajectory");
-    // let (coordinates, boxes) =
-    //     get_atoms_trj(trj);   // frames x atoms(3x1)
+    let trj = XTCTrajectory::open_read(trj).expect("Error reading trajectory");
+    let frames: Vec<Rc<Frame>> = trj.into_iter().map(|p| p.unwrap()).collect();
     // pbc whole 先不写, 先默认按照已经消除了周期性来做后续处理, 之后再看周期性的事
+    let (coordinates, boxes) = get_atoms_trj(frames);   // frames x atoms(3x1)
+    println!("{}", coordinates);
+    println!("{}", boxes);
     // 2. get charge, radius, LJ parameters of each atoms and generate qrv files
     let mut pid = String::from("_Protein_in_water");
     println!("Input system name (default: {}):", pid);
@@ -32,31 +35,28 @@ pub fn do_mmpbsa_calculations(trj: &String, mdp: &String, ndx: &Index, wd: &Path
     let qrv_path = qrv_path.to_str().unwrap();
     // gen_qrv(mdp, ndx, receptor_grp, ligand_grp, qrv_path, rad_type, rad_lj0);
     // 3. Mpdb>pqr, 输出apbs, 计算MM, APBS
-    do_mmpbsa(trj, mdp, ndx, qrv_path, wd, pid, complex_grp, receptor_grp, ligand_grp, settings);
+    // do_mmpbsa(trj, mdp, ndx, qrv_path, wd, pid, complex_grp, receptor_grp, ligand_grp, settings);
 }
 
-fn get_atoms_trj(trj: XTCTrajectory) -> (ArrayBase<OwnedRepr<[f32; 3]>, Dim<[Ix; 2]>>,
-                                         ArrayBase<OwnedRepr<[f32; 3]>, Dim<[Ix; 2]>>) {
-    let mut coord_matrix: Vec<[f32; 3]> = vec![];
-    let mut box_size: Vec<[f32; 3]> = vec![];
-    let mut num_frames = 0;
-    let mut num_atoms = 0;
-    for result in trj.into_iter() {
-        let frame = result.unwrap();
+fn get_atoms_trj(frames: Vec<Rc<Frame>>) -> (Array3<f64>, Array3<f64>) {
+    let num_frames = frames.len();
+    let num_atoms = frames[0].num_atoms();
+    let mut coord_matrix = Array3::<f64>::zeros((num_frames, num_atoms, 3));
+    let mut box_size = Array3::<f64>::zeros((num_frames, 3, 3));
+    for (idx, frame) in frames.into_iter().enumerate() {
         let atoms = frame.coords.to_vec();
-        for a in atoms {
-            coord_matrix.push(a);
+        for (i, a) in atoms.into_iter().enumerate() {
+            for j in 0..3 {
+                coord_matrix[[idx, i, j]] = a[j] as f64;
+            }
+
         }
-        for i in 0..3 {
-            box_size.push(frame.box_vector[i]);
+        for (i, b) in frame.box_vector.into_iter().enumerate() {
+            for j in 0..3 {
+                box_size[[idx, i, j]] = b[j] as f64;
+            }
         }
-        num_frames += 1;
-        num_atoms = frame.num_atoms();
     }
-    let coord_matrix = Array::from_shape_vec(
-        (num_frames, num_atoms), coord_matrix).unwrap();
-    let box_size = Array::from_shape_vec(
-        (num_frames, 3), box_size).unwrap();
     return (coord_matrix, box_size);
 }
 
@@ -73,56 +73,56 @@ fn do_mmpbsa(trj: &String, mdp: &String, ndx: &Index, qrv: &str, wd: &Path, pid:
     let df = &settings.df;
 
     // PB部分的参数从哪来?是否从前一菜单转移?
-    let PBEset = "\
-temp  298.15      # 温度\
-pdie  2           # 溶质介电常数\
-sdie  78.54       # 溶剂介电常数, 真空1, 水78.54\
-\
-npbe              # PB方程求解方法, lpbe(线性), npbe(非线性), smbpe(大小修正)\
-bcfl  mdh         # 粗略格点PB方程的边界条件, zero, sdh/mdh(single/multiple Debye-Huckel), focus, map\
-srfm  smol        # 构建介质和离子边界的模型, mol(分子表面), smol(平滑分子表面), spl2/4(三次样条/7阶多项式)\
-chgm  spl4        # 电荷映射到格点的方法, spl0/2/4, 三线性插值, 立方/四次B样条离散\
-swin  0.3         # 立方样条的窗口值, 仅用于 srfm=spl2/4\
-\
-srad  1.4         # 溶剂探测半径\
-sdens 10          # 表面密度, 每A^2的格点数, (srad=0)或(srfm=spl2/4)时不使用\
-\
-ion charge  1 conc 0.15 radius 0.95  # 阳离子的电荷, 浓度, 半径\
-ion charge -1 conc 0.15 radius 1.81  # 阴离子\
-\
-calcforce  no\
+    let PBEset = "
+temp  298.15      # 温度
+pdie  2           # 溶质介电常数
+sdie  78.54       # 溶剂介电常数, 真空1, 水78.54
+
+npbe              # PB方程求解方法, lpbe(线性), npbe(非线性), smbpe(大小修正)
+bcfl  mdh         # 粗略格点PB方程的边界条件, zero, sdh/mdh(single/multiple Debye-Huckel), focus, map
+srfm  smol        # 构建介质和离子边界的模型, mol(分子表面), smol(平滑分子表面), spl2/4(三次样条/7阶多项式)
+chgm  spl4        # 电荷映射到格点的方法, spl0/2/4, 三线性插值, 立方/四次B样条离散
+swin  0.3         # 立方样条的窗口值, 仅用于 srfm=spl2/4
+
+srad  1.4         # 溶剂探测半径
+sdens 10          # 表面密度, 每A^2的格点数, (srad=0)或(srfm=spl2/4)时不使用
+
+ion charge  1 conc 0.15 radius 0.95  # 阳离子的电荷, 浓度, 半径
+ion charge -1 conc 0.15 radius 1.81  # 阴离子
+
+calcforce  no
 calcenergy comps";
-    let PBAset = "\
-temp  298.15 # 温度\
-srfm  sacc   # 构建溶剂相关表面或体积的模型\
-swin  0.3    # 立方样条窗口(A), 用于定义样条表面\
-\
-# SASA\
-srad  1.4    # 探测半径(A)\
-gamma 1      # 表面张力(kJ/mol-A^2)\
-\
-#gamma const 0.0226778 3.84928\
-#gamma const 0.027     0\
-#gamma const 0.0301248 0         # AMBER-PB4 .0072*cal2J 表面张力, 常数\
-\
-press  0     # 压力(kJ/mol-A^3)\
-bconc  0     # 溶剂本体密度(A^3)\
-sdens 10\
-dpos  0.2\
-grid  0.1 0.1 0.1\
-\
-# SAV\
-#srad  1.29      # SAV探测半径(A)\
-#press 0.234304  # 压力(kJ/mol-A^3)\
-\
-# WCA\
-#srad   1.25           # 探测半径(A)\
-#sdens  200            # 表面的格点密度(1/A)\
-#dpos   0.05           # 表面积导数的计算步长\
-#bconc  0.033428       # 溶剂本体密度(A^3)\
-#grid   0.45 0.45 0.45 # 算体积分时的格点间距(A)\
-\
-calcforce no\
+    let PBAset = "
+temp  298.15 # 温度
+srfm  sacc   # 构建溶剂相关表面或体积的模型
+swin  0.3    # 立方样条窗口(A), 用于定义样条表面
+
+# SASA
+srad  1.4    # 探测半径(A)
+gamma 1      # 表面张力(kJ/mol-A^2)
+
+#gamma const 0.0226778 3.84928
+#gamma const 0.027     0
+#gamma const 0.0301248 0         # AMBER-PB4 .0072*cal2J 表面张力, 常数
+
+press  0     # 压力(kJ/mol-A^3)
+bconc  0     # 溶剂本体密度(A^3)
+sdens 10
+dpos  0.2
+grid  0.1 0.1 0.1
+
+# SAV
+#srad  1.29      # SAV探测半径(A)
+#press 0.234304  # 压力(kJ/mol-A^3)
+
+# WCA
+#srad   1.25           # 探测半径(A)
+#sdens  200            # 表面的格点密度(1/A)
+#dpos   0.05           # 表面积导数的计算步长
+#bconc  0.033428       # 溶剂本体密度(A^3)
+#grid   0.45 0.45 0.45 # 算体积分时的格点间距(A)
+
+calcforce no
 calcenergy total";
     let qrv = wd.join(pid.to_string() + ".qrv");
     println!("{}", qrv.display());
@@ -165,7 +165,6 @@ calcenergy total";
     let mut idx = 0;
     for line in &qrv[Atyp + 2..qrv.len() - 1] { // there's a blank line at the end
         let line: Vec<&str> = line
-            .trim()
             .split(" ")
             .filter_map(|p| match p.len() {
                 0 => None,
@@ -184,12 +183,69 @@ calcenergy total";
             resLig[idx] = "L~".to_string() + line[line.len() - 3];
         }
         idx += 1;
-        println!("{:?}", Qatm);
-        println!("{:?}", Ratm);
-        println!("{:?}", Catm);
-        println!("{:?}", Satm);
-        println!("{:?}", Eatm);
-        println!("{:?}", resPro);
-        println!("{:?}", resLig);
     }
+
+    // atom number of receptor and ligand
+    let Npro = resPro.len();
+    let Nlig = resLig.len();
+    let Ncom = Npro + Nlig;
+    let mut temp = 0.0;
+    let mut pdie = 0.0;
+    let mut sdie = 0.0;
+    let mut Nion: usize = 0;
+    let mut Qion = Array1::<f64>::zeros(10);
+    let mut Cion = Array1::<f64>::zeros(10);
+
+    // polar parameters
+    for i in PBEset.split("\n") {
+        if i.trim().len() != 0 {
+            let paras: Vec<&str> = i
+                .split(" ")
+                .filter_map(|p| match p.len() {
+                    0 => None,
+                    _ => Some(p)
+                })
+                .collect();
+            match paras[0] {
+                "temp" => temp = paras[1].parse().unwrap(),
+                "pdie" => pdie = paras[1].parse().unwrap(),
+                "sdie" => sdie = paras[1].parse().unwrap(),
+                "ion" => {
+                    Nion += 1;
+                    Qion[Nion] = paras[2].parse().unwrap();
+                    Cion[Nion] = paras[4].parse().unwrap();
+                }
+                _ => ()
+            }
+        }
+    }
+
+    let Iion = &Cion.dot(&(&Qion * &Qion));
+    let eps0 = 8.854187812800001e-12;
+    let kb = 1.380649e-23;
+    let Na = 6.02214076e+23;
+    let qe = 1.602176634e-19;
+    let RT2kJ = 8.314462618 * temp / 1e3;
+    let kap = 1e-10 / f64::sqrt(eps0 * kb * temp * sdie / (Iion * qe * qe * Na * 1e3));
+
+    let mut gamma = 0.0;
+    let mut _const = 0.0;
+    // apolar parameters
+    for i in PBAset.split("\n") {
+        if i.trim().len() != 0 {
+            let paras: Vec<&str> = i
+                .split("\n")
+                .filter_map(|p| match p.len() {
+                    0 => None,
+                    _ => Some(p)
+                })
+                .collect();
+            if paras[0].ends_with("gamma") && paras[1] == "const" {
+                gamma = paras[2].parse().unwrap();
+                _const = paras[3].parse().unwrap();
+            }
+        }
+    }
+
+
 }
