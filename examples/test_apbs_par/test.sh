03f2c3b9bcf52b404d5c99396afc4c30f3a1af23
Supernova <Jiaxing_Zhang@outlook.com>
2024-04-19 18:08:54 +0800

    Chenge parallel test

--- examples/test_apbs_par/test.sh
+++ examples/test_apbs_par/test.sh
@@ -1,16 +1,16 @@
 rm cost*
-for i in 1 2 4 8 16 32 64 128
+for i in 1 2 4 6 8 10 12 14 16 18 20 32 64 128
 do
     export OMP_NUM_THREADS=$i
     for j in `seq 1 3`
     do
         echo "Running with $i kernels the "$j"th time"
-        (time apbs _system_0ns.apbs) 1>/dev/null 2>> cost_$i.txt
+        (time apbs test.apbs) 1>/dev/null 2>> cost_$i.txt
     done
 done
 
 echo -e "cores,Time (s)" > cost.csv
-for i in 1 2 4 8 16 32 64 128
+for i in 1 2 4 6 8 10 12 14 16 18 20 32 64 128
 do
     for v in $(awk '/real/ {print $2;}' cost_$i.txt | awk -F "m" '{print $1 * 60 + $2;}')
     do
