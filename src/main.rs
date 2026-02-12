98cab414568a026975b3b47367a2f3e3a6f748e0
Supernova <Jiaxing_Zhang@outlook.com>
2026-02-12 16:04:54 +0800

    change data

--- src/main.rs
+++ src/main.rs
@@ -69,7 +69,7 @@ struct Cli {
 
 fn main() {
     let cli = Cli::parse();
-    let compile_date = "2025.01.28";
+    let compile_date = "2025/02/12";
     welcome(&env!("CARGO_PKG_VERSION"), compile_date);
     
     // Show version info
